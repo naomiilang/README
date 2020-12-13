1cf2bbf6b710ba88288c3e6a4a012994af95e412
Naomi <naomilang1212@gmail.com>
2020-12-13 12:41:00 -0800

    user prompts in terminal

--- Develop/index.js
+++ Develop/index.js
@@ -1,7 +1,96 @@
-// array of questions for user
-const questions = [
+// install inquierer 
+const inquirer = require('inquirer');
 
-];
+
+// array of questions for user
+const questions = () => {
+    return inquirer.prompt([
+        {
+            type: 'input',
+            message: 'What is your GitHub username?',
+            name: 'user',
+            validate: userInput => {
+                if (userInput) {
+                    return true;
+                } else {
+                    console.log('Please enter your Github username!');
+                    return false;
+                }
+            }
+        },
+        {
+            type: 'input',
+            message: 'What is your email address?',
+            name: 'email',
+            validate: emailInput => {
+                if (emailInput) {
+                    return true;
+                } else {
+                    console.log('Please enter your email!');
+                    return false;
+                }
+            }
+        },
+        {
+            type: 'input',
+            message: 'What is the title of your project?',
+            name: 'title',
+            validate: titleInput => {
+                if (titleInput) {
+                    return true;
+                } else {
+                    console.log('Please enter your project title!');
+                    return false;
+                }
+            }
+        },
+        {
+            type: 'input',
+            message: 'Enter a short description of your project',
+            name: 'project',
+            validate: projectInput => {
+                if (projectInput) {
+                    return true;
+                } else {
+                    console.log('Please enter your project description!');
+                    return false;
+                }
+            }
+        },
+        {
+            type: 'checkbox',
+            name: 'license',
+            message: 'What type of license would you like to include? (Choose one)',
+            choices: ['Apache License 2.0', 'GNU General Public License v3.0', 'MIT License', 'BSD 2-Clause "Simplified" License', 'BSD 3-Clause "New" or "Revised" License', 'Boost Software License 1.0', 'Creative Commons Zero v1.0 Universal', 'Eclipse Public License 2.0', 'GNU Affero General Public License v3.0', 'GNU General Public License v2.0', 'GNU Lesser General Public License v2.1', 'Mozilla Public License 2.0', 'The Unlicense']
+        },
+        {
+            type: 'input',
+            message: 'Enter installation instructions.',
+            name: 'install',
+        },
+        {
+            type: 'input',
+            message: 'Enter project usage information.',
+            name: 'usage',
+        },
+        {
+            type: 'input',
+            message: 'Enter project contribution guidelines',
+            name: 'contribute',
+        },
+        {
+            type: 'input',
+            message: 'Enter test instructions here',
+            name: 'test',
+        },
+        {
+            type: 'input',
+            message: 'What is your GitHub username?',
+            name: 'user',
+        },
+    ]);
+};
+questions();
 
 // function to write README file
 function writeToFile(fileName, data) {
